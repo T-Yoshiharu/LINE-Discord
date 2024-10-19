d34de2d8a3101b841a4cd6e4e8879b49bdd1e959
T-Yoshiharu <tinpani138.haru@gmail.com>
2024-10-20 01:11:17 +0900

    sendImage_v2.1

--- image.js
+++ image.js
@@ -1,14 +1,3 @@
-//lineから送信された画像をバイナリデータで取得
-function getImage(url) {
-    return UrlFetchApp.fetch(url, {
-        "method": "get",
-        'headers': {
-            'Content-Type': 'application/json; charset=UTF-8',
-            'Authorization': 'Bearer ' + TOKEN
-        }
-    });
-}
-
 //バイナリデータの画像をgyazoに送信し、urlを取得
 function gyazoup(image, idjson) {
     const options = {
