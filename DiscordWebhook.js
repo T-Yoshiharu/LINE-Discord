ccb3d902b0ec4d3df70fe0cd368f3f0aea3620fa
T-Yoshiharu <tinpani138.haru@gmail.com>
2024-10-22 16:31:39 +0900

    mention_v1 #LINEでのメンションをDiscordにも反映

--- DiscordWebhook.js
+++ DiscordWebhook.js
@@ -1,10 +1,19 @@
 function sendDiscordMessage(name, message, json) {
   // ウェブフック
   var webhookURL = json.Discord_hook;
+
   // Discord webhookに投げるメッセージの内容
-  var options = {
-    "content": name + " ; " + message
+  // 自分の個人メンションがある場合はメンションを付ける
+  if (message.includes("@Haru")) {
+    var options = {
+      "content": `<@${json.MyDiscord_ID}>\n` + name + " ; " + message
+    };
+  } else {
+    var options = {
+      "content": name + " ; " + message
+    };
   };
+
   // データを作って投げる
   var response = UrlFetchApp.fetch(
     webhookURL,
